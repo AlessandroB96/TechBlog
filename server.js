3dab7db484effbcbfb2366b048347dd0c2eb96ed
AlessandroB96 <abelaj7196@gmail.com>
2022-03-23 19:52:59 -0400

    added user and post routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
 const path = require('path');
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
